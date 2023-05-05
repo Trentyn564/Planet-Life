4e6dcac540c08cb2c02168f2a216e35aad6afc3f
Trentyn564 <116597220+Trentyn564@users.noreply.github.com>
2023-05-05 16:14:30 -0400

    Added features

---
+++ README.m
@@ -0,0 +1,32 @@
+# Planet-Life-Cheat
+Planet Life hasn't been hacked until now.
+
+# Notes:
+- First Planet Life "hack". (ALSO WORKS WITH SUPER PLANET LIFE)
+- Takes advantage of Base64 encryption & decryption.
+- Provided a ***.planet*** file for the original website.
+- To use this on [cool-math-games](https://coolmathgames.com) you'll need to edit the "state" local application varriable (With Base64).
+- To use this on [Planet Life](https://www.planetlife.space/play) or The [Full-Screen Version](https://www.planetlife.space/static/game/index.html) you'll probably want to import the provided *.planet file.
+- [This Link](//www.coolmathgames.com/sites/default/files/public_games/39874/) forces the game to open in Fullscreen on [cool-math-games](https://coolmathgames.com).
+
+# Features:
+- Infinite Gold
+- Infinite Wood
+- Infinite Stardust
+- Infinite Coco
+
+# Editing The Cheat:
+- Decode the Base64 String with [Base64-Decode](https://www.base64decode.org/) or [Planet-Life Save Converter](https://github.com/Cracko298/Planet-Life-Save-Converter/releases/tag/v1.1-alpha-1)
+- Edit the decrypted Base64-String or ***.planet*** file. Then load it or convert it back to the original format.
+
+# What is The [Planet-Life Save Converter](https://github.com/Cracko298/Planet-Life-Save-Converter/releases/tag/v1.1-alpha-1)?
+- Converts the Save-Data Base64-String to a normal ***.planet*** file.
+- Can converts the ***.planet*** file to a Save-Data Base64-String.
+
+# Credits:
+- Created By: [Cracko298](https://github.com/Cracko298)
+- Please [Star](https://github.com/Cracko298/Planet-Life-Cheat/stargazers) the Repository or [Fork](https://github.com/Cracko298/Planet-Life-Cheat/fork) it, this helps a ton!
+
+![image](https://user-images.githubusercontent.com/78656905/138370958-50497b46-d567-4b14-b22f-97b80a65c3eb.png)
+![image](https://user-images.githubusercontent.com/78656905/197082881-7e4fe773-dcb3-472b-9cb2-d62b201efa71.png)
+![image](https://user-images.githubusercontent.com/78656905/197082983-668939f5-2a1a-4831-be10-3bf84336db97.png)
